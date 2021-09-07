f4b41b5446b7d09da1001a919e73061f05fa9098
josephpchen6 <90228930+josephpchen6@users.noreply.github.com>
2021-09-07 17:28:35 -0400

    Update MacOSXPlaylistToCSV.py

--- MacOSXPlaylistToCSV.py
+++ MacOSXPlaylistToCSV.py
@@ -19,7 +19,7 @@ pl_lengthRequest = youtube.playlists().list(
     )
 pl_lengthResponse = pl_lengthRequest.execute()
 
-for g in pl_lengthResponse['items']: 
+for a in pl_lengthResponse['items']: 
     plCount.append(g['contentDetails']['itemCount']) #gets number of playlist items in list format
 playlistLength = ' '.join(map(str, plCount))
 num = (int(playlistLength)-1) // 5      #turns list into string and divides by 5 for later use
@@ -71,7 +71,7 @@ for count in range(int(num)):
         )
     pageKey_response = pageKey_request.execute()
 
-    for a in pl_response['items']: 
+    for b in pl_response['items']: 
         vid_ids.append(a['contentDetails']['videoId'])  #gets list of video IDs off the playlist's videos
 
     idCopy = vid_ids       #creates a copies of the video ID list for processing
@@ -90,15 +90,15 @@ for count in range(int(num)):
         )
     title_response = title_request.execute()    #gets snippet off a video
 
-    for b in title_response['items']:
+    for c in title_response['items']:
         titles.append(b['snippet']['localized']['title'])   #gets the title of a video from the video ID list copy
                       
-    for c in stat_response['items']:
-        views.append(c['statistics']['viewCount'])      #gets the view count of a video from the video ID list copy
-        likes.append(c['statistics']['likeCount'])      #also gets the like count of the video
+    for d in stat_response['items']:
+        views.append(d['statistics']['viewCount'])      #gets the view count of a video from the video ID list copy
+        likes.append(d['statistics']['likeCount'])      #also gets the like count of the video
 
-    for d in pageKey_response['nextPageToken']:
-        token = token + d
+    for e in pageKey_response['nextPageToken']:
+        token = token + e
     #print(token)       #remove for debug
     
     csvTitles.extend(titles)
@@ -119,8 +119,8 @@ remainderId_request = youtube.playlistItems().list(
     )
 remainderId_response = remainderId_request.execute()        #manual function for the last page with no "nextPageToken'
     
-for e in remainderId_response['items']:
-    extraIds.append(e['contentDetails']['videoId'])
+for f in remainderId_response['items']:
+    extraIds.append(f['contentDetails']['videoId'])
     
 remainderTitle_request = youtube.videos().list(
     part = 'snippet',
@@ -130,8 +130,8 @@ remainderTitle_response = remainderTitle_request.execute()
 
 extraIdCopy = extraIds
 
-for f in remainderTitle_response['items']:
-    extraTitles.append(f['snippet']['localized']['title'])
+for g in remainderTitle_response['items']:
+    extraTitles.append(g['snippet']['localized']['title'])
 
 remainderStat_request = youtube.videos().list(
     part = 'statistics',
@@ -139,9 +139,9 @@ remainderStat_request = youtube.videos().list(
     )
 remainderStat_response = remainderStat_request.execute()
 
-for g in remainderStat_response['items']:
-    extraViews.append(g['statistics']['viewCount'])
-    extraLikes.append(g['statistics']['likeCount'])
+for h in remainderStat_response['items']:
+    extraViews.append(h['statistics']['viewCount'])
+    extraLikes.append(h['statistics']['likeCount'])
 
 csvTitles.extend(extraTitles)
 csvViews.extend(extraViews)
@@ -151,7 +151,7 @@ csvLikes.extend(extraLikes)
 #extraDebug()
 
 likesPerView = [float(l)/float(v) for l,v in zip(csvLikes, csvViews)]
-csvLikesPerView = [round (h, 4) for h in likesPerView]      #divides the like list and the view list
+csvLikesPerView = [round (i, 4) for i in likesPerView]      #divides the like list and the view list
 
 fileMake = open('PlaylistData.csv', 'w+')
 
@@ -159,7 +159,7 @@ filePath = os.path.abspath(fileMake.name)
 
 fileMake.close()
 
-csvLinks = [urlHead + i for i in urlList]   #makes the video ID's clickable links
+csvLinks = [urlHead + j for j in urlList]   #makes the video ID's clickable links
     
 dict = {'Video Title': csvTitles, 'Views': csvViews, 'Likes/View': csvLikesPerView, 'Video Links':csvLinks}
 df = pd.DataFrame(dict)
